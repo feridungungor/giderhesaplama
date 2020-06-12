260834ae65408c7701f01abc33c4e3ebd5aff8ce
feridungungor <feridungungor26@gmail.com>
2020-06-12 05:18:56 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Gider Hesaplama"),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Text(
+            "Yeni Projeye Hoş Geldiniz",
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
+    );
+  }
+}
